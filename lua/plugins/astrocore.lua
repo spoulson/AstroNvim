d2033307b1ab5e438e0b2aa17bcf13a3e93513a7
Shawn Poulson <spoulson@mailgun.com>
2024-11-25 12:00:15 -0500

    Autosize signcolumn.

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -28,15 +28,15 @@ return {
         relativenumber = false, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        signcolumn = "auto:9", -- sets vim.opt.signcolumn
         wrap = true, -- sets vim.opt.wrap
-        scrolloff = 10
+        scrolloff = 10,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
-        switch_mapping = "-"
+        switch_mapping = "-",
       },
     },
     -- Mappings can be configured through AstroCore as well.
